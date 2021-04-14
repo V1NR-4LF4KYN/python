77ff9ee2a8c9d9c0459498803df4466b3c4994be
V1NR-4LF4KYN <caturix29@gmail.com>
2021-04-14 15:06:48 +0200

    wrote a working code in mouse.py

--- mouse.py
+++ mouse.py
@@ -2,6 +2,11 @@ import pyautogui as p
 
 try:
     print(p.position())
-    p.move(50, 50)
+    x, y = p.size()
+    print(f'Size of screen: {x}px x {y}px')
+    p.moveTo(x/2, y/2, 1)
+    print(f'Current pos: {p.position()}')
+    #p.move(50, 50, 1)
+
 except:
     print("aborted")
