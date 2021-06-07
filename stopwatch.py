1f868d3855a87f6f10cd4e9581d15dac47a46cbf
V1NR-4LF4KYN <caturix29@gmail.com>
2021-06-07 07:32:45 +0200

    Figure out way to stop time measuring when paused.

--- stopwatch.py
+++ stopwatch.py
@@ -8,7 +8,6 @@
 # 06.06.2021
 
 
-# TODO: figure out way to stop time measuring when paused. 
 
 
 import time
@@ -17,9 +16,11 @@ from pynput.keyboard import Key, Listener
 
 class Stopwatch:
 	def __init__(self):
-		self.time = 0
-		self.START_TIME = time.time()			# Time at start of program
+		self.time: float = 0					# Total Time, includes all seconds since start so you can pause
+		self.START_TIME: float = 0				# Time at start of program
+		self.current_time: float = 0 			# Current Time while stopping it	
 		self.running: bool = False				# Bool to control pause of watch
+		self.flipped: bool = False				# switches to True when unpaused
 
 		self.listener = Listener(self.on_press) # listener for keyboard input
 		self.listener.start()					# starts listener
@@ -32,22 +33,22 @@ class Stopwatch:
 			else:
 				self.running = True
 
-	def stopTime(self):							# if not paused print time
-		#while True:								# ctrl+c 'able due to try-except-block because of endless loop
-		#	try:
-		#		if self.running:			
-		#			print(f'{time.time() - self.START_TIME:.3f}') # rounding it to 3 decimal points
-		#			time.sleep(0.001)
-		#	except:	
-		#		print('Something went wrong. Exiting...')
-		#		break
-		while True:
+	def stopTime(self):						# if not paused print time
+		while True:							# ctrl+c 'able due to try-except-block because of endless loop
 			try:
-				if self.running:
-					print(f'{self.time:.3f} s')
-					self.time += 0.01
-					time.sleep(0.01)
-			except:
+				if self.running:			# if unpaused 
+					if not self.flipped:	# first set start time one more time. Then stop. Only does it when flipped is False
+						self.START_TIME = time.time()	
+						self.flipped = True	
+					self.current_time = (time.time() - self.START_TIME) # current time is time right then minus the start time
+					print(f'{self.current_time + self.time:.3f}') # printing current time + all other times we measured beefore AND rounding it to 3 decimal points
+					time.sleep(0.001) # after each step sleep 0.001 secs
+				else: 
+					self.flipped = False					# reset flip when paused
+					self.time += self.current_time			# add current time to total time
+					self.current_time = 0 					# current time = 0, so loop doesnt add more
+			except:	
+				print('Something went wrong. Exiting...')
 				break
 
 
